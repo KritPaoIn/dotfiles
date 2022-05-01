db831a9e78846b894ea25778c337677dbc416bca
KritPaoIn <kritpaointara@gmail.com>
2022-05-01 09:08:08 +0700

    Fixed install.sh and updated .gitignore

--- .tmux/.tmux
+++
@@ -1,1 +0,0 @@
-/Users/krit/.dotfiles/.tmux
--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 if [ ! -z $HOME ]; then
     mkdir -p $HOME/.config/
-    ln -sF $HOME/.dotfiles/.config/nvim $HOME/.config/nvim
-    ln -sF $HOME/.dotfiles/.config/fish $HOME/.config/fish
-    ln -sF $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
-    ln -sF $HOME/.dotfiles/.tmux $HOME/.tmux
-    ln -sF $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+    ln -sF $HOME/.dotfiles/.config/nvim $HOME/.config
+    ln -sF $HOME/.dotfiles/.config/fish $HOME/.config
+    ln -sF $HOME/.dotfiles/.tmux.conf $HOME
+    ln -sF $HOME/.dotfiles/.tmux $HOME
+    ln -sF $HOME/.dotfiles/.gitconfig $HOME
 fi
